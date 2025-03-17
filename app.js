94a4a4db2c5900470df5519b5f76f80d9f961d94
Igorrocha1 <rocha-igor@hotmail.com>
2025-03-17 20:30:24 -0300

    adicionando lista de amigos e laço de repetição

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function alertaDeListaVazia() {
+    document.getElementById('alertaDeListaVazia').removeAttribute('hidden');
+}
+
+function esconderAlertaDeListaVazia() {
+    document.getElementById('esconderAlertaDeListaVazia').setAttribute('hidden',true);
+}
+
+function adicionarAmigo() {
+  let novoAmigo = document.getElementById('amigo').value;
+  if (novoAmigo === '') {
+    alertaDeListaVazia();
+  } else {
+    listaDeAmigos.push(novoAmigo);
+    document.getElementById('amigo').value = '';
+    console.log(listaDeAmigos);
+  }
+
+}
+
+function mostrarListaDeAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+
+    listaAmigos.innerHTML = '';
+   
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = listaDeAmigos[i];
+        li.appendChild(li);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function adicionarAmigo() {
+//     document.getElementById('amigo').innerHTML = listaDeAmigos.push();
+//     console.log(listaDeAmigos);
+
+// }
+
+// function gerarNomeAleatorio(){
+//     let amigo = parseInt(Math.random() * listaDeAmigos.length, 10);
+//     if (amigo.includes)
+// }
