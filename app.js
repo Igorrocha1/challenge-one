ac208bc06cf5bbecbd6e26661d49f629d873a01b
Igorrocha1 <rocha-igor@hotmail.com>
2025-03-17 22:50:22 -0300

    Adicionando função de exibir resultado e sortear amigo

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
 let listaDeAmigos = [];
 
 function alertaDeListaVazia() {
-    document.getElementById('alertaDeListaVazia').removeAttribute('hidden');
+  document.getElementById("alertaDeListaVazia").removeAttribute("hidden");
 }
 
 function esconderAlertaDeListaVazia() {
-    document.getElementById('esconderAlertaDeListaVazia').setAttribute('hidden');
+  document.getElementById("esconderAlertaDeListaVazia").setAttribute("hidden");
 }
 
 function adicionarAmigo() {
-  let novoAmigo = document.getElementById('amigo').value;
-  if (novoAmigo === '') {
+  let novoAmigo = document.getElementById("amigo").value;
+  if (novoAmigo === "") {
     alertaDeListaVazia();
   } else {
     listaDeAmigos.push(novoAmigo);
-    document.getElementById('amigo').value = '';
+    document.getElementById("amigo").value = "";
     mostrarListaDeAmigos();
     console.log(listaDeAmigos);
   }
-
 }
 
 function mostrarListaDeAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
 
-        let listaAmigos = document.getElementById('listaAmigos');
-        listaAmigos.innerHTML = '';
+  for (let i = 0; i < listaDeAmigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = listaDeAmigos[i];
+    listaAmigos.appendChild(li);
+  }
+}
 
-            for (let i = 0; i < listaDeAmigos.length; i++) {
-                let li = document.createElement('li');
-                li.textContent = listaDeAmigos[i];
-                listaAmigos.appendChild(li);
-            }
+function sortearAmigo() {
+  if (listaDeAmigos.length < 2) {
+    alert("a lista esta vazia");
+  } else {
+    let numeroDaListaSorteado = Math.floor(
+      Math.random() * listaDeAmigos.length
+    );
+    mostrarResultadoSorteio(listaDeAmigos[numeroDaListaSorteado]);
+    listaDeAmigos.splice(numeroDaListaSorteado, 1);
+  }
+}
 
-    }
+function mostrarResultadoSorteio(amigoSorteado) {
+  let resultadoAmigoSecreto = document.getElementById("resultado");
+  resultadoAmigoSecreto.innerHTML = "";
+
+  let li = document.createElement("li");
+  li.textContent = `Seu amigo secreto é: ${amigoSorteado}`;
+  resultadoAmigoSecreto.appendChild(li);
+}
